e143d29cc8a3daee8cab303909ccb9bc4640ef34
iantsaraz <iantsaraz02@gmail.com>
2021-10-14 09:26:36 +0300

    m

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -21,29 +21,3 @@
 // const FireBase = firebase.initializeApp(firebaseConfig);
 // export default Firebase;
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-
-import { getStorage } from "firebase/storage";
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-	apiKey: "AIzaSyDpSGRNrj25eKC5d8VxWluZS1_K-1ZemWY",
-	authDomain: "texttest-b2e31.firebaseapp.com",
-	projectId: "texttest-b2e31",
-	storageBucket: "texttest-b2e31.appspot.com",
-	messagingSenderId: "980260726335",
-	appId: "1:980260726335:web:8c50cf1d97a2a79538fa8a",
-	measurementId: "G-4THGX3GNH3",
-};
-
-// Initialize Firebase
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-var storage = firebase.storage();
-export default storage;
